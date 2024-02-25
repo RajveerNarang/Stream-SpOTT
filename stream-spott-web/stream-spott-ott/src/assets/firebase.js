34a74b54687623beded79de6009456a3c592f66a
Rajveer Narang <60134939+RajveerNarang@users.noreply.github.com>
2024-02-25 21:43:23 +0530

    Update firebase.js
    changes final

--- stream-spott-web/stream-spott-ott/src/assets/firebase.js
+++ stream-spott-web/stream-spott-ott/src/assets/firebase.js
@@ -1,8 +1,18 @@
-
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
 
+const firebaseConfig = {
+  // Your Firebase configuration
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
 
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const storageRef = ref(storage);
